d0935355a647591d5c036efa8d8ff2f0964ed010
Sanskar Instep <code@insteptechnologies.com>
2024-04-08 12:47:40 +0530

    added express.json() to parse body

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ const express = require('express')
 const routes = require('./routes')
 const app = express();
 
+// parse json req body
+app.use(express.json())
+// parse the url
+app.use(express.urlencoded({ extended: true }))
 
 app.use('/', routes)
 app.listen(3000, () => {
